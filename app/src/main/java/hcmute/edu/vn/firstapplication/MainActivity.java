1a6709bd449fb8d98179f0522c2b38ab4ce5f63c
GNReplay <52163755+GNReplay@users.noreply.github.com>
2023-02-10 16:03:18 +0700

    Relativelayouttest

--- app/src/main/java/hcmute/edu/vn/firstapplication/MainActivity.java
+++ app/src/main/java/hcmute/edu/vn/firstapplication/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.linearlayouttest);
+        setContentView(R.layout.relativelayouttest);
     }
 }
